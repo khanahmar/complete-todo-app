38c495f8e6396084c68a4b200bc90462a49749ea
Ahmar khan <ahmarbilal223344@gmail.com>
2022-12-31 14:28:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("from");
+const input = document.getElementById("input");
+const todosUl = document.getElementById("todos");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo();
+});
+
+function addTodo(todo){
+    let 
+}
