392ff85c307affa6062e5ab7bd2b767736b3288e
Ahmar khan <ahmarbilal223344@gmail.com>
2022-12-31 15:13:01 +0500

    completed

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUl = document.getElementById("todos");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   addTodo();
@@ -20,18 +26,34 @@ function addTodo(todo) {
     }
     todoEl.innerText = todoText;
 
-    todoEl.addEventListener("click", (e) =>
-      todoEl.classList.toggle("completed")
-    );
+    todoEl.addEventListener("click", (e) => {
+      todoEl.classList.toggle("completed");
+      updateLS();
+    });
 
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
 
       todoEl.remove();
+      updateLS();
     });
 
     todosUl.appendChild(todoEl);
 
     input.value = "";
+
+    updateLS();
   }
+}
+
+function updateLS() {
+  todoEl = document.querySelectorAll("li");
+  const todos = [];
+  todoEl.forEach((todo) => {
+    todos.push({
+      text: todo.innerText,
+      completed: todo.classList.contains("completed"),
+    });
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
